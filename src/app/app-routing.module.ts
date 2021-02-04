e31acb898c6a481622ee0e3f61960ca966579564
alicelg <alicelop@ucm.es>
2021-02-04 12:24:10 +0100

    añado rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EducatorsComponent } from './educators/educators.component';
+import { HomeComponent } from './home/home.component';
+import { ParentsComponent } from './parents/parents.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'usuario', component: UserComponent },
+  { path: 'padres', component: ParentsComponent },
+  { path: 'educadores', component: EducatorsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
