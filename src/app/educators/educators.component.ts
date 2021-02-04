b2361a6dafe393ca77a7291e74d74ea5562b64f5
alicelg <alicelop@ucm.es>
2021-02-04 12:21:09 +0100

    añado componente educators

---
+++ src/app/educators/educators.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-educators',
+  templateUrl: './educators.component.html',
+  styleUrls: ['./educators.component.scss']
+})
+export class EducatorsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
