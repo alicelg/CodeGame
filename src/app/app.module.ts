bbf6287b8cd71aae95a0d29c94762f61e7bc4797
alicelg <alicelop@ucm.es>
2021-02-04 12:21:45 +0100

    añado componente user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { UserComponent } from './user/user.component';
+import { FooterComponent } from './footer/footer.component';
+import { ParentsComponent } from './parents/parents.component';
+import { EducatorsComponent } from './educators/educators.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    FooterComponent,
+    ParentsComponent,
+    EducatorsComponent
   ],
   imports: [
     BrowserModule,
