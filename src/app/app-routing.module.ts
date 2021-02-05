799b487e82c9588ec5fde2f4a01b7c0322a4724b
alicelg <alicelop@ucm.es>
2021-02-05 12:46:10 +0100

    añado ruta para login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EducatorsComponent } from './educators/educators.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 import { ParentsComponent } from './parents/parents.component';
 import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'empezar', component: LoginComponent },
   { path: 'usuario', component: UserComponent },
   { path: 'padres', component: ParentsComponent },
   { path: 'educadores', component: EducatorsComponent }
