c11449ba7782dc76a410807a791340edf8bb31c4
alicelg <alicelop@ucm.es>
2021-02-05 12:21:25 +0100

    añado routa home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'usuario', component: UserComponent },
   { path: 'padres', component: ParentsComponent },
   { path: 'educadores', component: EducatorsComponent }
